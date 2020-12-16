26c7b757f618d65613a6572b4d01fd635dcd1b69
beomjun <os130607@naver.com>
2020-12-16 14:30:30 +0900

    로딩, 로그인 디자인 완료

--- app/src/main/java/com/example/sd2020/LoginActivity.java
+++ app/src/main/java/com/example/sd2020/LoginActivity.java
@@ -41,6 +41,9 @@ public class LoginActivity extends AppCompatActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        this.overridePendingTransition(R.anim.fade_in,R.anim.fade_out);
+
         setContentView(R.layout.activity_login);
         sf=getSharedPreferences("saved",0);
         db=FirebaseDatabase.getInstance();
